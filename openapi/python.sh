d913c360f06fd48b704dd5dd8cbc025ea859b3bd
rnetser <rnetser@redhat.com>
2022-08-07 18:50:16 +0300

    Updated client

--- openapi/python.sh
+++ openapi/python.sh
@@ -1,3 +1,17 @@
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/bin/bash
 
 set -o errexit
@@ -30,7 +44,7 @@ source "${SCRIPT_ROOT}/openapi-generator/client-generator.sh"
 source "${SETTING_FILE}"
 OPENAPI_GENERATOR_COMMIT="${OPENAPI_GENERATOR_COMMIT:-v6.0.0}"
 
-CLEANUP_DIRS=("${PACKAGE_NAME}/api" "${PACKAGE_NAME}/apis" "${PACKAGE_NAME}/models" docs); \
+CLEANUP_DIRS=("${PACKAGE_NAME}/api" "${PACKAGE_NAME}/apis" "${PACKAGE_NAME}/models" "${PACKAGE_NAME}/model" docs); \
 ocmclient::generator::generate_client "${OUTPUT_DIR}"
 
 echo "---Done."
